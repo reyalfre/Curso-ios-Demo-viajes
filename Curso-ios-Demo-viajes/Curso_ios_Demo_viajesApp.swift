76e3eb5ec952dc4e7ebf3d1d06fd80529f507755
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-01-29 10:06:08 +0100

    Initial Commit

---
+++ Curso-ios-Demo-viajes/Curso_ios_Demo_viajesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_Demo_viajesApp.swift
+//  Curso-ios-Demo-viajes
+//
+//  Created by Equipo 8 on 29/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_Demo_viajesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
